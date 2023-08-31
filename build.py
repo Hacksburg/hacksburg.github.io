400764ebee433214f677a261ed0d7f9fa9094547
Hunter Irving <hunter@hunterirving.com>
2023-08-30 21:04:25 -0400

    add ability to cancel events (#37)
    * add ability to cancel events
    
    ---------
    
    Co-authored-by: John Hutchinson <john@hacksburg.org>

--- build.py
+++ build.py
@@ -61,20 +61,20 @@ def json_to_html():
 
 	html = ""
 	for post in posts:
-		date = post["date"]
+		date = datetime.strptime(post["date"], '%Y-%m-%d')
 		start_time = post["start_time"]
 		end_time = post["end_time"]
+		cancelled = post["cancelled"]
 
+		# Header
 		html += f'\n\t\t\t\t\t<div class="post" data-isodate="{date}">\n'
 		html += '\t\t\t\t\t\t<div class="post_header">\n'
 		
 		if date and start_time and end_time:
-			calendar_day = datetime.strptime(post["date"], '%Y-%m-%d').day
 			html += f'\t\t\t\t\t\t\t<div class="calendar_link noselect">\n'
-			html += f'\t\t\t\t\t\t\t\t<div class="circled_date">{calendar_day}</div>\n'
-			
-			month_int = int(post["date"].split("-")[1])
-			month = datetime(month=month_int, day=1, year=1).strftime('%B')
+			html += f'\t\t\t\t\t\t\t\t<div class="circled_date">{date.day}</div>\n'
+
+			month = date.strftime('%B')
 			html += f'\t\t\t\t\t\t\t\t<div class="month_and_time">{month}<br>\n'
 			
 			time_string = start_time + " - " + end_time if start_time[-2:] != end_time[-2:] else start_time[:-2] + " - " + end_time
@@ -82,9 +82,13 @@ def json_to_html():
 			html += '\t\t\t\t\t\t\t\t</div>\n'
 			html += '\t\t\t\t\t\t\t</div>\n'
 
+		# Title
 		html += '\t\t\t\t\t\t\t<div class="title_and_subtitle_wrapper">\n'
 		html += '\t\t\t\t\t\t\t\t<div class="title_and_subtitle">\n'
-		html += f'\t\t\t\t\t\t\t\t\t<div class="title">{post["title"]}</div>\n'
+		html += f'\t\t\t\t\t\t\t\t\t<div class="title">'
+		if cancelled:
+			html += '(Cancelled) '
+		html += f'{post["title"]}</div>\n'
 		html += f'\t\t\t\t\t\t\t\t\t<div class="subtitle">{post["subtitle"]}</div>\n'
 		html += '\t\t\t\t\t\t\t\t</div>\n'
 		html += '\t\t\t\t\t\t\t</div>\n'
@@ -92,56 +96,71 @@ def json_to_html():
 		html += '\t\t\t\t\t\t\t\t<div class="x"></div>\n'
 		html += '\t\t\t\t\t\t\t</div>\n'
 		html += '\t\t\t\t\t\t</div>\n'
+
+		# Image
 		html += '\t\t\t\t\t\t<div class="closeable">\n'
 		html += '\t\t\t\t\t\t\t<div class="post_body">\n'
 		if post["image"]:
 			html += f'\t\t\t\t\t\t\t\t<img class="post_image" src="/resources/images/{post["image"]}" loading="lazy">\n'
-			html += f'\t\t\t\t\t\t\t\t<div class="post_text">{post["description"]}\n'
+		html += f'\t\t\t\t\t\t\t\t<div class="post_text"'
+		if post["image"]:
+			html += '>'
 		else:
-			html += f'\t\t\t\t\t\t\t\t<div class="post_text" style="width: 100%">{post["description"]}\n'
+			html += ' style="width: 100%">'
 		
-		html += '\t\t\t\t\t\t\t\t\t<p>\n'
-		if date and start_time and end_time:
-			date_obj = datetime.strptime(date, "%Y-%m-%d").date()
-			formatted_date_str = date_obj.strftime(f"%A, %B {date_obj.day}{date_suffix(date_obj.day)}")
-			html += f'\t\t\t\t\t\t\t\t\t<b>Time</b>: {formatted_date_str} from {time_string}<br>\n'
-			
-		location_str = ""
-		if post["offsite_location"]:
-			location_str = post["offsite_location"]
-		elif post["offered_in_person"] and post["offered_online"]:
-			location_str = 'Online and in person at Hacksburg; <a href="https://www.google.com/maps/dir/?api=1&destination=Hacksburg+Blacksburg,+VA" target="_blank">1872 Pratt Drive Suite 1620</a>.<br>\n\t\t\t\t\t\t\t\t\tEnter through the covered double doors at the back of the building.'
-		elif post["offered_in_person"]:
-			location_str = 'In person at Hacksburg; <a href="https://www.google.com/maps/dir/?api=1&destination=Hacksburg+Blacksburg,+VA" target="_blank">1872 Pratt Drive Suite 1620</a>. Enter through the covered double doors at the back of the building.'
-		elif post["offered_online"]:
-			location_str = 'Online only.'
-			
-		if location_str:
-			html += f'\t\t\t\t\t\t\t\t\t<b>Place</b>: {location_str}<br>\n'
-		
-		if post["offered_online"]:
-			html += '\t\t\t\t\t\t\t\t\t<b>URL</b>: '
-			html += '<a href="https://meet.hacksburg.org/class" target="_blank">meet.hacksburg.org/class</a><br>\n'
-
-		if post["member_price"] == 0 and post["non_member_price"] == 0:
-			html += '\t\t\t\t\t\t\t\t\t<b>Cost</b>: Free!\n'
-		elif post["member_price"] == 0:
-			html += f'\t\t\t\t\t\t\t\t\t<b>Cost</b>: Free for Hacksburg members; ${post["non_member_price"]} for non-members. Pay in person or online at <a href="https://paypal.me/hacksburg" target="_blank">paypal.me/hacksburg</a>.\n'
-		elif post["member_price"] == post["non_member_price"]:
-			html += f'\t\t\t\t\t\t\t\t\t<b>Cost</b>: ${post["non_member_price"]}. Pay in person or online at <a href="https://paypal.me/hacksburg" target="_blank">paypal.me/hacksburg</a>.\n'
+		# Description
+		if cancelled:
+			html += f'<s>{post["description"]}</s>\n'
+			html += '\t\t\t\t\t\t\t\t\t<br><p><b>This event has been cancelled.</b></p>\n'
 		else:
-			html += f'\t\t\t\t\t\t\t\t\t<b>Cost</b>: ${post["member_price"]} for Hacksburg members; ${post["non_member_price"]} for non-members. Pay in person or online at <a href="https://paypal.me/hacksburg" target="_blank">paypal.me/hacksburg</a>.\n'
-		html += '\t\t\t\t\t\t\t\t\t</p>\n'
-
-		meetup_link = post["meetup_link"]
+			html += f'{post["description"]}\n'
 		
+		# Event details (time, location, cost)
+		if not cancelled:
+			html += '\t\t\t\t\t\t\t\t\t<p>\n'
+			if date and start_time and end_time:
+				formatted_date_str = date.strftime(f"%A, %B %d{date_suffix(date.day)}")
+				html += f'\t\t\t\t\t\t\t\t\t<b>Time</b>: {formatted_date_str} from {time_string}<br>\n'
+			
+			location_str = ""
+			if post["offsite_location"]:
+				location_str = post["offsite_location"]
+			elif post["offered_in_person"] and post["offered_online"]:
+				location_str = 'Online and in person at Hacksburg; <a href="https://www.google.com/maps/dir/?api=1&destination=Hacksburg+Blacksburg,+VA" target="_blank">1872 Pratt Drive Suite 1620</a>.<br>\n\t\t\t\t\t\t\t\t\tEnter through the covered double doors at the back of the building.'
+			elif post["offered_in_person"]:
+				location_str = 'In person at Hacksburg; <a href="https://www.google.com/maps/dir/?api=1&destination=Hacksburg+Blacksburg,+VA" target="_blank">1872 Pratt Drive Suite 1620</a>. Enter through the covered double doors at the back of the building.'
+			elif post["offered_online"]:
+				location_str = 'Online only.'
+			
+			if location_str:
+				html += f'\t\t\t\t\t\t\t\t\t<b>Place</b>: {location_str}<br>\n'
+		
+			if post["offered_online"]:
+				html += '\t\t\t\t\t\t\t\t\t<b>URL</b>: '
+				html += '<a href="https://meet.hacksburg.org/class" target="_blank">meet.hacksburg.org/class</a><br>\n'
+
+			if post["member_price"] == 0 and post["non_member_price"] == 0:
+				html += '\t\t\t\t\t\t\t\t\t<b>Cost</b>: Free!\n'
+			elif post["member_price"] == 0:
+				html += f'\t\t\t\t\t\t\t\t\t<b>Cost</b>: Free for Hacksburg members; ${post["non_member_price"]} for non-members. Pay in person or online at <a href="https://paypal.me/hacksburg" target="_blank">paypal.me/hacksburg</a>.\n'
+			elif post["member_price"] == post["non_member_price"]:
+				html += f'\t\t\t\t\t\t\t\t\t<b>Cost</b>: ${post["non_member_price"]}. Pay in person or online at <a href="https://paypal.me/hacksburg" target="_blank">paypal.me/hacksburg</a>.\n'
+			else:
+				html += f'\t\t\t\t\t\t\t\t\t<b>Cost</b>: ${post["member_price"]} for Hacksburg members; ${post["non_member_price"]} for non-members. Pay in person or online at <a href="https://paypal.me/hacksburg" target="_blank">paypal.me/hacksburg</a>.\n'
+			html += '\t\t\t\t\t\t\t\t\t</p>\n'
+
+		# Meetup/RSVP link
 		if post["meetup_link"]:
-			html += f'\t\t\t\t\t\t\t\t\t<a class="button rsvp_button" href="{meetup_link}" target="_blank">RSVP on Meetup</a>\n'
-			html += '\t\t\t\t\t\t\t\t\t<div class="below_button_text">\n'
-			subject = f'RSVP for {post["title"]}'
-			body = f'I am confirming my RSVP for \"{post["title"]}\" on {formatted_date_str} from {time_string}.'
-			html += f'\t\t\t\t\t\t\t\t\t\tor <a href="{build_mailto(subject, body)}" target="_blank">RSVP by Email</a>\n'
-			html += '\t\t\t\t\t\t\t\t\t</div>\n'
+			if not cancelled:
+				html += f'\t\t\t\t\t\t\t\t\t<a class="button rsvp_button" href="{post["meetup_link"]}" target="_blank">RSVP on Meetup</a>\n'
+				html += '\t\t\t\t\t\t\t\t\t<div class="below_button_text">\n'
+				subject = f'RSVP for {post["title"]}'
+				body = f'I am confirming my RSVP for \"{post["title"]}\" on {formatted_date_str} from {time_string}.'
+				html += f'\t\t\t\t\t\t\t\t\t\tor <a href="{build_mailto(subject, body)}" target="_blank">RSVP by Email</a>\n'
+				html += '\t\t\t\t\t\t\t\t\t</div>\n'
+
+			else:
+				html += f'\t\t\t\t\t\t\t\t\t<a class="button rsvp_button disabled" href="{post["meetup_link"]}" target="_blank">View on Meetup</a>\n'
 		
 		html += '\t\t\t\t\t\t\t\t</div>\n'
 		html += '\t\t\t\t\t\t\t</div>\n'
